9f266a1105d4832ad3e5f4536c8ecb81fcda6d89
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2023-02-17 00:01:03 +0300

    change

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -19,7 +19,7 @@ class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
     @Test
